748ef4909a36346f9c3afd7254773930d58230a8
Dauren <qdauren@gmail.com>
2022-04-11 01:03:35 -0400

    classification optimization + saved models

--- Source/train_models_classification.py
+++ Source/train_models_classification.py
@@ -1,10 +1,12 @@
-import os
 import pickle
 import numpy as np
 import pandas as pd
+import warnings
 
 import xgboost as xgb
 import lightgbm as lgb
+from skopt import BayesSearchCV
+from skopt.space import Integer, Categorical
 
 from tqdm import tqdm
 from sklearn.metrics import classification_report, confusion_matrix
@@ -12,6 +14,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from utils import classify_columns
 
+warnings.filterwarnings('ignore')
 availability_df = pd.read_csv("Data/Preprocessed_data/hawaii_cat.csv")
 
 train_df, val_df = train_test_split(availability_df,
@@ -65,8 +68,54 @@ for model, model_name in tqdm(default_models):
     d = d[col_order]
     temp1.append(d)
 
+
+# Model optimization
+rf_cls = RandomForestClassifier(random_state=1234)
+
+search_spaces = {'max_depth': Categorical([5, 8, 15, 25, 30, None]),
+                 'min_samples_split': Integer(1, 100),
+                 'min_samples_leaf': Integer(1, 10),
+                 'n_estimators': Integer(100, 1200),
+                 'max_features': Categorical(['log2', 'sqrt', None])}
+
+opt_rf = BayesSearchCV(rf_cls,
+                       search_spaces,
+                       scoring='accuracy',
+                       cv=3,
+                       n_iter=5,
+                       return_train_score=True,
+                       refit=True,
+                       optimizer_kwargs={'base_estimator': 'GP'},
+                       random_state=42)
+
+print("Starting optimization... Takes a lot of time...")
+opt_rf.fit(x_train, y_train)
+preds = opt_rf.predict(x_val)
+cm = confusion_matrix(y_val, preds)
+cm = pd.DataFrame(cm)
+cm['model'] = 'random forest optimized'
+temp2.append(cm)
+rep = classification_report(y_val, preds, output_dict=True)
+d = pd.DataFrame(rep).transpose().reset_index()
+d['model'] = 'random forest optimized'
+d = d[col_order]
+temp1.append(d)
+
 results_df = pd.concat(temp1, axis=0)
 results_df.to_csv("Data/clf_model_val_results.csv", index=False)
 conf_m_df = pd.concat(temp2, axis=0)
 conf_m_df.columns = ['true class 0', 'true class 1', 'true class 2', 'model']
 conf_m_df.to_csv("Data/clf_val_conf_matrix.csv", index=False)
+
+best_params = dict(opt_rf.best_params_)
+X = pd.concat([x_train, x_val], axis=0)
+y = pd.concat([y_train, y_val], axis=0)
+rf_cls = RandomForestClassifier(random_state=1234, **best_params)
+rf_cls.fit(X, y)
+
+with open("Models/rf_clf.pickle", "wb") as f:
+    pickle.dump(rf_cls, f)
+
+xgb_cls = xgb.XGBClassifier(random_state=1234)
+xgb_cls.fit(X, y)
+xgb_cls.save_model("Models/xgb_cls.json")
