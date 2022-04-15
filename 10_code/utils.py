61f4247f784d5859548560080b573a5f18ce0b37
Yuting Chiu (Cindy) <89810916+cindy-yuting-chiu@users.noreply.github.com>
2022-04-15 19:14:33 -0400

    Update utils.py

--- 10_code/utils.py
+++ 10_code/utils.py
@@ -1,7 +1,16 @@
 import pandas as pd
+### Helper functions to import in other files 
 
 
 def classify_columns(x_train: pd.DataFrame, x_val: pd.DataFrame):
+    """
+    input: 
+    x_train: dataframe for training data
+    x_val: dataframe for validation set
+    return:
+    x_train: processed dataframe for training data
+    x_val: processed dataframe for validation data
+    """
     categorical_features = [
         'host_response_time', 'room_type', 'host_has_profile_pic',
         'host_identity_verified', 'bathrooms_text', 'bedrooms',
@@ -17,10 +26,13 @@ def classify_columns(x_train: pd.DataFrame, x_val: pd.DataFrame):
     feature_names = categorical_features + numerical_features
     feature_types = ['categorical' for _ in range(len(categorical_features))] + \
                     ['continuous' for _ in range(len(numerical_features))]
+    # convert the text version columns (categorical columns) to int
     x_train[categorical_features] = x_train[categorical_features].astype(int)
     x_val[categorical_features] = x_val[categorical_features].astype(int)
+    # the rest of the columns stay the same 
     x_train = x_train[feature_names]
     x_val = x_val[feature_names]
+    # convert the numerical columns to float
     x_train[numerical_features] = x_train[numerical_features].astype(float)
     x_val[numerical_features] = x_val[numerical_features].astype(float)
     return x_train, x_val
@@ -28,9 +40,8 @@ def classify_columns(x_train: pd.DataFrame, x_val: pd.DataFrame):
 
 def naive_predictions(df: pd.DataFrame):
     """
-
-    :param df:
-    :return:
+    Generate Baseline model for our availabiity classification 
+    Generate prediction using mean of the price
     """
     return df.groupby(['month'], as_index=False)[['log_price_mean']].mean()
 
